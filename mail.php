a4d8602cfbb4557426d59dcec7be47c271feef8e
Hrach <khrach116@gmail.com>
2025-11-16 02:22:02 +0400

    new project

---
+++ mail.php
@@ -0,0 +1,67 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$statusMessage = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'levonbakunts3@gmail.com';
+        $mail->Password = 'fago gfkl muqh vfjg';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom($email, 'Website Form');
+        $mail->addAddress('levonbakunts96@gmail.com', 'Levon Bakunts');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'New Message from Website Form';
+        $bodyContent = "
+            <html>
+                <head>
+                    <style>
+                        table { border-collapse: collapse; width: 100%; }
+                        table, th, td { border: 1px solid black; }
+                        th, td { padding: 8px; text-align: left; }
+                    </style>
+                </head>
+                <body>
+                    <h2>New Message from Website</h2>
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <td>" . htmlspecialchars($name) . "</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>" . htmlspecialchars($email) . "</td>
+                        </tr>
+                        <tr>
+                            <th>Message</th>
+                            <td>" . nl2br(htmlspecialchars($message)) . "</td>
+                        </tr>
+                    </table>
+                </body>
+            </html>";
+        $mail->Body = $bodyContent;
+        $mail->AltBody = "You have a new message from $name ($email).\nMessage: $message";
+
+        $mail->send();
+        $statusMessage = 'success';
+    } catch (Exception $e) {
+        $statusMessage = 'error';
+    }
+    header("Location: index.html?status=$statusMessage");
+    exit;
+}
