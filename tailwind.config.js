e50c8579566b6efbca43376a56526fbc3eb2e2d7
Garik <garikgevorgyan.rap@mail.ru>
2025-11-17 00:43:59 +0400

    remove node modules

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // 🔑 This is the most crucial part for class generation
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Make sure this covers all your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
